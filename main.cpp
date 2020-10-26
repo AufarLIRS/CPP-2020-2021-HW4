3ab7d36ee84fd97e3974877b287ac35616c3a607
Ramir Sultanov <ramirrsultanov@stud.kpfu.ru>
2020-10-26 16:22:17 +0300

    upgrade to O(nlogn)

--- main.cpp
+++ main.cpp
@@ -4,50 +4,54 @@
 #include <algorithm>
 #include <string>
 
-using namespace std;
-
 void task_1()
 {
-  const string answer_1 = "CHAT WITH HER!";
-  const string answer_2 = "IGNORE_HIM!";
-  string input = "";
-  cin >> input;
-  cout << ((set<char>(input.begin(), input.end()).size() % 2 == 0) ? answer_1 : answer_2) << endl;
+  const std::string answer_1 = "CHAT WITH HER!";
+  const std::string answer_2 = "IGNORE_HIM!";
+  std::string input = "";
+  std::cin >> input;
+  std::cout << ((std::set<char>(input.begin(), input.end()).size() % 2 == 0) ? answer_1 : answer_2) << std::endl;
 }
 
 void task_2()
 {
-  const int number_of_compilations = 2;
-  vector<int> fixed_errors;
+  const int number_of_errors = 2;
   auto read_integers_to_multiset_from_console = [](int n) {
-    multiset<int>* ms = new multiset<int>;
-    int tmp = 0;
+    std::multiset<int> ms;
     for (int i = 0; i < n; i++)
     {
-      cin >> tmp;
-      ms->insert(tmp);
+      int tmp = 0;
+      std::cin >> tmp;
+      ms.insert(tmp);
     }
     return ms;
   };
   int input_1 = 0;
-  cin >> input_1;
-  vector<multiset<int>*> errors{ read_integers_to_multiset_from_console(input_1) };
-  for (int i = 1; i <= number_of_compilations; i++)
+  std::cin >> input_1;
+  std::multiset<int> input_2 = read_integers_to_multiset_from_console(input_1);
+  for (int i = 0; i < number_of_errors; ++i)
   {
-    errors.push_back(read_integers_to_multiset_from_console(input_1 - i));
-    set_difference(errors[i - 1]->begin(), errors[i - 1]->end(), errors[i]->begin(), errors[i]->end(),
-                   inserter(fixed_errors, fixed_errors.begin()));
-    cout << *fixed_errors.begin() << endl;
+    std::string tmp = "";
+    std::getline(std::cin, tmp);
+  }
+  std::multiset<int> input_last = read_integers_to_multiset_from_console(input_1 - number_of_errors);
+  std::for_each(input_last.begin(), input_last.end(), [&input_2](const int& n) {
+    auto pos = input_2.find(n);
+    input_2.erase(pos);
+  });
+  for (int i : input_2)
+  {
+    std::cout << i << std::endl;
   }
 }
 
 void task_3()
 {
-  string input = "abcd";
+  std::string input = "abcd";
   std::sort(input.begin(), input.end());
   do
   {
-    cout << input << endl;
+    std::cout << input << std::endl;
   } while (std::next_permutation(input.begin(), input.end()));
 }
 
