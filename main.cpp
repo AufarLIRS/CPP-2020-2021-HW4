6bf157f952ce36ef656fc0cb1a91cb41f8c2325f
Kamil Nazmiev <kamilotina2001@gmail.com>
2020-10-28 20:40:50 +0300

    First commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,59 @@
 #include <iostream>
-
-using namespace std;
+#include <algorithm>
+#include <set>
+#include <map>
+#include <string>
+#include <functional>
+#include <iomanip>
+#include <list>
+#include <vector>
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  // First task
+  std::string input;
+  std::cin >> input;
+  if (input.size() % 2 == 0)
+    std::cout << "CHAT WITH HER!" << '\n';
+  else
+    std::cout << "IGNORE HIM!" << '\n';
+
+  // Second task
+  int count;
+  std::cin >> count;
+  std::multiset<int> firstset;
+  std::multiset<int> secondset;
+  std::multiset<int> thirdset;
+  std::vector<int> result(2);
+  std::vector<int>::iterator it;
+
+  for (int i = 0; i < count; i++)
+  {
+    int temp;
+    std::cin >> temp;
+    firstset.insert(temp);
+  }
+  for (int i = 0; i < count - 1; i++)
+  {
+    int temp;
+    std::cin >> temp;
+    secondset.insert(temp);
+  }
+  for (int i = 0; i < count - 2; i++)
+  {
+    int temp;
+    std::cin >> temp;
+    thirdset.insert(temp);
+  }
+  //Обошёлся без второго массива, но вводить всё-таки нужно
+  it = std::set_difference(firstset.begin(), firstset.end(), thirdset.begin(), thirdset.end(), result.begin());
+  for (it = result.begin(); it != result.end(); ++it)
+    std::cout << *it << std::endl;
+
+  // Third task
+  std::string s = "abcd";
+  do
+  {
+    std::cout << s << '\n';
+  } while (std::next_permutation(s.begin(), s.end()));
 }
