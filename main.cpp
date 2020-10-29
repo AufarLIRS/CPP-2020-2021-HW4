47ed39104254fd4924a69ee2c1ac60dcdc48c55b
alaedindouak <alaedin.hak@gmail.com>
2020-10-29 11:23:14 +0300

    solve first and third task

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <algorithm>
+#include <set>
+#include <string>
 
-using namespace std;
+
+void task_1(std::string username)
+{
+    std::set<char> chars(username.begin(), username.end());
+
+    if(chars.size() % 2 == 0)
+        std::cout << "CHAT WITH " << username << std::endl;
+    else
+        std::cout << "IGNORE HIM!" << std::endl;
+}
+
+void task_2();
+
+void task_3(std::string chars)
+{
+    do
+    {
+        std::cout << chars << std::endl;
+    }while (std::next_permutation(chars.begin(), chars.end()));
+
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+//    task_1("sevenkplus");
+//    task_2();
+//    task_3("abcd");
     return 0;
 }
