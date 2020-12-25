95f42c9f9c0660331f445fe483d9348c58a6b132
Urazalin Mirlan <uraz.mir@gmail.com>
2020-12-26 02:29:56 +0500

    отформатировал код

--- main.cpp
+++ main.cpp
@@ -3,55 +3,55 @@
 #include <algorithm>
 #include <vector>
 
-
 int main()
 {
-    //task 1
-    std::set <char> set;
-    std::string name;
-    std::cin >> name;
-    for(unsigned i = 0; i < name.size(); i++) {
-        set.insert(name[i]);
-    }
-    if(set.size() % 2 != 0) {
-        std::cout << "IGNORE HIM!" << std::endl;
-    }
-    else{
-        std::cout << "CHAT WITH HER!" << std::endl;
-    }
+  // task 1
+  std::set<char> set;
+  std::string name;
+  std::cin >> name;
+  for (unsigned i = 0; i < name.size(); i++)
+  {
+    set.insert(name[i]);
+  }
+  if (set.size() % 2 != 0)
+  {
+    std::cout << "IGNORE HIM!" << std::endl;
+  }
+  else
+  {
+    std::cout << "CHAT WITH HER!" << std::endl;
+  }
 
-    //task 2
-    int n,temp;
-        std::cin >> n;
-        long a=0,b=0,c=0;
-        for(int i=0;i<n;i++){
-            std::cin >> temp;
-            a= a + temp;
-        }
-        for(int i=0;i<n-1;i++){
-            std::cin >> temp;
-            b+=temp;
-        }
+  // task 2
+  int n, temp;
+  std::cin >> n;
+  long a = 0, b = 0, c = 0;
+  for (int i = 0; i < n; i++)
+  {
+    std::cin >> temp;
+    a = a + temp;
+  }
+  for (int i = 0; i < n - 1; i++)
+  {
+    std::cin >> temp;
+    b += temp;
+  }
 
-        for(int i=0;i<n-2;i++){
-            std::cin >> temp;
-            c+=temp;
-        }
-        std::cout << a-b << std::endl;
-        std::cout << b-c << std::endl;
+  for (int i = 0; i < n - 2; i++)
+  {
+    std::cin >> temp;
+    c += temp;
+  }
+  std::cout << a - b << std::endl;
+  std::cout << b - c << std::endl;
 
-
-
-
-
-
-    //task 3
-    char abcd[] = "abcd";
-    do {
+  // task 3
+  char abcd[] = "abcd";
+  do
+  {
     std::cout << abcd << '\t';
-    }
-    while (std::next_permutation(abcd, abcd + 4));
-    std::cout << std::endl;
+  } while (std::next_permutation(abcd, abcd + 4));
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
