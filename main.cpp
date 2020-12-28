9e61b282173b46109554d3449f6ae06479820ba1
Alaedindouak <alaedin.hak@gmail.com>
2020-12-29 02:21:08 +0300

    add task_2

--- main.cpp
+++ main.cpp
@@ -14,7 +14,55 @@ void task_1(std::string username)
         std::cout << "IGNORE HIM!" << std::endl;
 }
 
-void task_2();
+void task_2()
+{
+    unsigned short n;
+    std::cin >> n;
+
+    std::multiset<unsigned short> first;
+    std::multiset<unsigned short> second;
+    std::multiset<unsigned short> third;
+
+    for(unsigned short i = 0; i < n; i++)
+    {
+        unsigned short a;
+        std::cin >> a;
+        first.insert(a);
+    }
+
+    for (unsigned short i = 0; i < n - 1; i++)
+    {
+        unsigned short a;
+        std::cin >> a;
+        second.insert(a);
+    }
+
+    for (unsigned short i = 0; i < n - 2; i++)
+    {
+        short a;
+        std::cin >> a;
+        third.insert(a);
+    }
+
+    for (auto it = first.begin(); it != first.end(); ++it)
+    {
+        if (second.count(*it) < first.count(*it))
+        {
+          std::cout << *it << std::endl;
+          break;
+        }
+    }
+
+    for (auto it = second.begin(); it != second.end(); ++it)
+    {
+        if (third.count(*it) < second.count(*it))
+        {
+          std::cout << *it << std::endl;
+          break;
+        }
+    }
+
+}
 
 void task_3(std::string chars)
 {
@@ -28,7 +76,7 @@ void task_3(std::string chars)
 int main()
 {
 //    task_1("sevenkplus");
-//    task_2();
+    task_2();
 //    task_3("abcd");
     return 0;
 }
